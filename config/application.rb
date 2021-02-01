cdd66d91a80a9476cde2d45a7a189bc87ba215bc
KyosukeYanagihara <rctttzg7@gmail.com>
2021-02-01 14:50:58 +0900

    日本語設定

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ module Divegram
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators do |g|
