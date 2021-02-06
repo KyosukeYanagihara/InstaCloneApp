94e2cb7f927370b22dba028bcd845e7c1b988da5
KyosukeYanagihara <rctttzg7@gmail.com>
2021-02-06 15:23:45 +0900

    Userのseedデータ投入コード

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |n|
+  password = "password"
+  User.create!(name: Faker::Artist.name,
+               email: Faker::Internet.email,
+               password: password,
+               password_confirmation: password,
+               image: open("#{Rails.root}/db/fixtures/icon#{n}.png")
+               )
+end
